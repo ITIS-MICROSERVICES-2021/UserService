a630193f30aec739ff3af0c59bc6eb4304ee936e
PrepConcede <tagmirs_letter@mail.ru>
2021-09-23 01:02:07 +0300

    #1 ctorp

--- UserService.Core/Entities/User.cs
+++ UserService.Core/Entities/User.cs
@@ -7,27 +7,43 @@ namespace UserService.Core.Entities
         protected User()
         {
         }
-        
+
+        public User(string surname, string name, string patronymic, string position, string managerFullName,
+            string companyFullName)
+        {
+            Surname = surname;
+            Name = name;
+            Patronymic = patronymic;
+            Position = position;
+            ManagerFullName = managerFullName;
+            CompanyFullName = companyFullName;
+        }
+
         /// <summary>
         /// Фамилия
         /// </summary>
         public string Surname { get; protected set; }
+
         /// <summary>
         /// Имя
         /// </summary>
         public string Name { get; protected set; }
+
         /// <summary>
         /// Отчество
         /// </summary>
         public string Patronymic { get; protected set; }
+
         /// <summary>
         /// Должность
         /// </summary>
         public string Position { get; protected set; }
+
         /// <summary>
         /// ФИО генерального директора
         /// </summary>
         public string ManagerFullName { get; protected set; }
+
         /// <summary>
         /// Полное наименование компании
         /// </summary>
