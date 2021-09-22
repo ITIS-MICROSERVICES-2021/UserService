26c5f90c4cb602711a0922445d0bfcb22bd9f29c
PrepConcede <tagmirs_letter@mail.ru>
2021-09-23 00:55:55 +0300

    #1 User entity

--- UserService.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace UserService.Core
-{
-    public class Class1
-    {
-    }
-}
---
+++ UserService.Core/Entities/User.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UserService.Core.Entities
+{
+    public class User : IdentityUser<long>
+    {
+        protected User()
+        {
+        }
+        
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string Surname { get; protected set; }
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name { get; protected set; }
+        /// <summary>
+        /// Отчество
+        /// </summary>
+        public string Patronymic { get; protected set; }
+        /// <summary>
+        /// Должность
+        /// </summary>
+        public string Position { get; protected set; }
+        /// <summary>
+        /// ФИО генерального директора
+        /// </summary>
+        public string ManagerFullName { get; protected set; }
+        /// <summary>
+        /// Полное наименование компании
+        /// </summary>
+        public string CompanyFullName { get; protected set; }
+    }
+}
