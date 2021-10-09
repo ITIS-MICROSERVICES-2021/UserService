ab4c4a81db964416ae4b143290ea6543e1129a47
Grigorev Yakov <yaschok315@gmail.com>
2021-10-09 17:12:56 +0300

    Update CreateUserValidationTest.cs

--- UserService.Test.Unit/CreateUserValidationTest.cs
+++ UserService.Test.Unit/CreateUserValidationTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using UserService.Features.UserManagement.Create;
 
 namespace UserService.Test.Unit
@@ -6,6 +7,19 @@ namespace UserService.Test.Unit
     [TestFixture]
     public class CreateUserTest
     {
+        private static CreateUserInputDto Dto => new()
+        {
+            Surname = nameof(CreateUserInputDto.Surname),
+            Name = nameof(CreateUserInputDto.Name),
+            Patronymic = nameof(CreateUserInputDto.Patronymic),
+            Position = nameof(CreateUserInputDto.Position),
+            ManagerFullName = nameof(CreateUserInputDto.ManagerFullName),
+            CompanyName = nameof(CreateUserInputDto.CompanyName),
+            Salary = 15400,
+            RecruitmentDate = DateTime.UtcNow.Date,
+            TelegramId = Guid.NewGuid()
+        };
+        
         private CreateUserValidator Validator { get; set; }
         
         [SetUp]
@@ -17,88 +31,85 @@ namespace UserService.Test.Unit
         [Test]
         public void ValidationFailsIfSurnameIsNull()
         {
-            var dto = new CreateUserInputDto
-            {
-                Name = "Name",
-                Patronymic = "Patronymic",
-                Position = "Position",
-                ManagerFullName = "ManagerFullName",
-                CompanyName = "CompanyName"
-            };
-            CheckWhetherIsPropertyCorrect(dto);
+            var dto = GenerateWrongDto<string>(nameof(CreateUserInputDto.Surname), null);
+            CheckIfPropertyIsCorrect(dto);
         }
         
         [Test]
         public void ValidationFailsIfNameIsNull()
         {
-            var dto = new CreateUserInputDto
-            {
-                Surname = "Surname",
-                Patronymic = "Patronymic",
-                Position = "Position",
-                ManagerFullName = "ManagerFullName",
-                CompanyName = "CompanyName"
-            };
-            CheckWhetherIsPropertyCorrect(dto);
+            var dto = GenerateWrongDto<string>(nameof(CreateUserInputDto.Name), null);
+            CheckIfPropertyIsCorrect(dto);
         }
         
         [Test]
         public void ValidationFailsIfPatronymicIsNull()
         {
-            var dto = new CreateUserInputDto
-            {
-                Name = "Name",
-                Surname = "Surname",
-                Position = "Position",
-                ManagerFullName = "ManagerFullName",
-                CompanyName = "CompanyName"
-            };
-            CheckWhetherIsPropertyCorrect(dto);
+            var dto = GenerateWrongDto<string>(nameof(CreateUserInputDto.Patronymic), null);
+            CheckIfPropertyIsCorrect(dto);
         }
         
         [Test]
         public void ValidationFailsIfPositionIsNull()
         {
-            var dto = new CreateUserInputDto
-            {
-                Name = "Name",
-                Surname = "Surname",
-                Patronymic = "Patronymic",
-                ManagerFullName = "ManagerFullName",
-                CompanyName = "CompanyName"
-            };
-            CheckWhetherIsPropertyCorrect(dto);;
+            var dto = GenerateWrongDto<string>(nameof(CreateUserInputDto.Position), null);
+            CheckIfPropertyIsCorrect(dto);;
         }
         
         [Test]
         public void ValidationFailsIfManagerFullNameIsNull()
         {
-            var dto = new CreateUserInputDto
-            {
-                Name = "Name",
-                Surname = "Surname",
-                Patronymic = "Patronymic",
-                Position = "Position",
-                CompanyName = "CompanyName"
-            };
-            CheckWhetherIsPropertyCorrect(dto);
+            var dto = GenerateWrongDto<string>(nameof(CreateUserInputDto.ManagerFullName), null);
+            CheckIfPropertyIsCorrect(dto);
         }
         
         [Test]
         public void ValidationFailsIfCompanyNameIsNull()
         {
-            var dto = new CreateUserInputDto
-            {
-                Name = "Name",
-                Surname = "Surname",
-                Patronymic = "Patronymic",
-                Position = "Position",
-                ManagerFullName = "ManagerFullName"
-            };
-            CheckWhetherIsPropertyCorrect(dto);
+            var dto = GenerateWrongDto<string>(nameof(CreateUserInputDto.CompanyName), null);
+            CheckIfPropertyIsCorrect(dto);
         }
 
-        private void CheckWhetherIsPropertyCorrect(CreateUserInputDto dto)
+        private CreateUserInputDto GenerateWrongDto<T>(string incorrectPropertyName, T incorrectValue)
+        {
+            var dto = Dto;
+            switch (incorrectPropertyName)
+            {
+                case nameof(CreateUserInputDto.Surname):
+                    dto.Surname = incorrectValue as string;
+                    break;
+                case nameof(CreateUserInputDto.Name):
+                    dto.Name = incorrectValue as string;
+                    break;
+                case nameof(CreateUserInputDto.Patronymic):
+                    dto.Patronymic = incorrectValue as string;
+                    break;
+                case nameof(CreateUserInputDto.Position):
+                    dto.Position = incorrectValue as string;
+                    break;
+                case nameof(CreateUserInputDto.ManagerFullName):
+                    dto.ManagerFullName = incorrectValue as string;
+                    break;
+                case nameof(CreateUserInputDto.CompanyName):
+                    dto.CompanyName = incorrectValue as string;
+                    break;
+                case nameof(CreateUserInputDto.Salary):
+                    dto.Salary = Convert.ToInt64(incorrectValue);
+                    break;
+                case nameof(CreateUserInputDto.RecruitmentDate):
+                    dto.RecruitmentDate = Convert.ToDateTime(incorrectValue);
+                    break;
+                case nameof(CreateUserInputDto.TelegramId):
+                    dto.TelegramId = (Guid)(incorrectValue as object);
+                    break;
+                default:
+                    throw new ArgumentException("Parameter incorrectPropertyName isn't right");
+            }
+
+            return dto;
+        }
+
+        private void CheckIfPropertyIsCorrect(CreateUserInputDto dto)
         {
             var command = new CreateUserCommand(dto);
             var result = Validator.Validate(command);
